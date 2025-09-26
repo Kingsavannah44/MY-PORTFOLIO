4c2e0b2d6f8bb0ecfd38a2da75e3deed519200c7
Kingsavannah44 <shadsbrian@gmail.com>
2025-09-26 15:48:15 +0300

    Initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
-import backend from "./backend.png";
+import backend from "./company/backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -26,8 +25,8 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
+import election from "./election.png";
+import Image from "./Image.png";
 import tripguide from "./tripguide.png";
 
 export {
@@ -56,7 +55,7 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
-  jobit,
+  election,
+  Image,
   tripguide,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -19,8 +19,8 @@ import {
   starbucks,
   tesla,
   shopify,
-  carrent,
-  jobit,
+  election,
+  Image,
   tripguide,
   threejs,
 } from "../assets";
@@ -120,7 +120,7 @@ const experiences = [
     company_name: "Starbucks",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2025- April 2025",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -133,7 +133,7 @@ const experiences = [
     company_name: "Tesla",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "Jan 2025- Feb 2025",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -146,7 +146,7 @@ const experiences = [
     company_name: "Shopify",
     icon: shopify,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "Feb 2025 - May 2025",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -159,7 +159,7 @@ const experiences = [
     company_name: "Meta",
     icon: meta,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "May 2025 -present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -198,16 +198,16 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "CAMPUS VOTING SYSTEM",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to vote , register to vote, and manage campus elections effectively, providing a convenient and efficient solution for conducting campus elections.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "MySQL",
         color: "green-text-gradient",
       },
       {
@@ -215,29 +215,29 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
+    image: election,
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "LOCATION TRACKER",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Web application that enables users to track, monitor the real time, and locate a historical location of an object,vehicle or a person.",
     tags: [
       {
-        name: "react",
+        name: "python",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "flaskapi",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "css",
         color: "pink-text-gradient",
       },
     ],
-    image: jobit,
-    source_code_link: "https://github.com/",
+    image: Image,
+    source_code_link: "https://github.com/Kingsavannah44/Location-tracker.git",
   },
   {
     name: "Trip Guide",
